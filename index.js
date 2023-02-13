62e5e234594fd1dc835bc57a0787ddcc1ae81b24
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-02-13 17:48:58 +0600

    Raw node project part 7 is runnintg

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 /**
- * Title: Uptime Monitoring Application
- * Description: A RESTFuL Api to monitor up or down time user defined links
+ * Title: Project Initial File
+ * Description: Initial file to start the node server and workers 
  * Author: Shakhawat Salam (learner)
- * Date: 02/03/2022  
+ * Date: 02/09/2022  
  */
 
-//dependenciss
-
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes.js');
-const environment = require('./helpers/environment');
+const server = require('./lib/server.js');
+const workers = require('./lib/worker.js');
 // const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notification.js');
 
 // app object - module scaffolding
 
 const app = {};
-
-
-
-sendTwilioSms('01876288562', 'Hello world', (err) => {
-    console.log(`this is the error`, err);
-});
 
 //configuration
 //@TODO: pore moche dibo
@@ -48,20 +38,17 @@ sendTwilioSms('01876288562', 'Hello world', (err) => {
 
 
 
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init()
+}
 
 
-//create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(process.env.NODE_ENV);
-        console.log(`listening to port ${environment.port}`);
-    });
-};
-
-//handle Request Response
-app.handleReqRes = handleReqRes;
+app.init();
 
 
-//start the server
-app.createServer();
+// export the app
+
+module.exports = app;
